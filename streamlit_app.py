db4beb8289c63320bf25ed0aacfcc71ff3a12b82
circe-wu <1448717625@qq.com>
2023-03-28 21:35:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,8 @@ if fruits_selected == EmptyList:
 else:
   fruits_to_show = my_fruit_list.loc[fruits_selected]
   streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
