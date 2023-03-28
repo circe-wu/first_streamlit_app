2dfaaef121b7e0848be15af3e1270a20d44f43b8
circe-wu <1448717625@qq.com>
2023-03-28 16:28:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Lime'])
 
-if fruits_selected) == EmptyList:
+if fruits_selected == EmptyList:
   streamlit.dataframe(my_fruit_list)
 else:
   fruits_to_show = my_fruit_list.loc[fruits_selected]
